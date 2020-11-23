f029876d15fbd197289d76a928b28896ce921fd6
Feng <yao.feng@accenture.com>
2020-11-23 10:46:17 +0800

    1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      test
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
