d4b54c3a26d18abb1874f6f977e056e7350ff5c5
Yao <fy.fenty@gmail.com>
2020-12-18 18:58:37 +0800

    1

---
+++ src/component/navigation/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navigation() {
+  return (
+    <ul>
+      <li>123</li>
+      <li>123</li>
+      <li>123</li>
+    </ul>
+  );
+}
---
+++ src/container/home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navigation from "../../component/navigation";
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <Navigation></Navigation>
+    )
+  }
+}
